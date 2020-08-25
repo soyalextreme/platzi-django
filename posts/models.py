3aa30c93f213827d3523d18ac42bcb3b03d794e7
Alejandro Andrade <alex.anso.17@gmail.com>
2020-08-24 22:15:33 -0500

    Two tables conected succesfully with the FK.

--- posts/models.py
+++ posts/models.py
@@ -5,9 +5,20 @@ Modelos del post
 from django.db import models
 
 
+class Countries(models.Model):
+    """Country Model"""
+
+    country_name = models.CharField(max_length=3)
+
+    
+
+
+
 
 class User(models.Model):
     """User Model"""
+
+    #Django adds default id to the class
 
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
@@ -18,10 +29,16 @@ class User(models.Model):
     bio = models.TextField(blank=True)
 
 
+    is_admin = models.BooleanField(default=False)
+
+
     birthday = models.DateField(blank=True, null=True)
 
     # DateTime Almacena tambien la hora
     # cuando se crea 
     created_at = models.DateTimeField(auto_now_add=True)
-    # cuando se modifica
+    # cuando se modifica
     modified_at = models.DateTimeField(auto_now=True)
+    
+    city = models.CharField(max_length=100, default="NO REGISTERS")
+    country = models.ForeignKey(Countries, on_delete="SET NULL", null=True)
